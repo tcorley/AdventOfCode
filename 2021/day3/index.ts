64d593c5f6a461161d05b700ae8f3301b6fdb635
Tyler Corley <tcorley@linkedin.com>
2021-12-03 13:14:37 -0800

    Remove comments

--- 2021/day3/index.ts
+++ 2021/day3/index.ts
@@ -32,28 +32,21 @@ export const partTwo = (data: string) => {
   let oxygenData: string[] = JSON.parse(JSON.stringify(formattedData));
   let bitIndex = 0;
   while (1) {
-    // console.log("-------------------");
-    // console.log("before: ", oxygenData);
     const mostCommon =
       oxygenData.reduce((acc, curr) => (acc += parseInt(curr[bitIndex])), 0) >=
       oxygenData.length / 2
         ? "1"
         : "0";
-    // console.log("most common: ", mostCommon);
     oxygenData = oxygenData.filter((row) => row[bitIndex] === mostCommon);
-    // console.log("after: ", oxygenData);
     bitIndex++;
     if (oxygenData.length === 1) {
       break;
     }
   }
-  console.log(`oxygen rating is ${oxygenData[0]}`);
 
   let c02RatingData: string[] = JSON.parse(JSON.stringify(formattedData));
   bitIndex = 0;
   while (1) {
-    // console.log("-------------------");
-    // console.log("before: ", c02RatingData);
     const leastCommon =
       c02RatingData.reduce(
         (acc, curr) => (acc += parseInt(curr[bitIndex])),
@@ -62,17 +55,14 @@ export const partTwo = (data: string) => {
       c02RatingData.length / 2
         ? "0"
         : "1";
-    // console.log("least common: ", leastCommon);
     c02RatingData = c02RatingData.filter(
       (row) => row[bitIndex] === leastCommon
     );
-    // console.log("after: ", c02RatingData);
     bitIndex++;
     if (c02RatingData.length === 1) {
       break;
     }
   }
-  console.log(`C02 rating is ${c02RatingData[0]}`);
 
   return parseInt(c02RatingData[0], 2) * parseInt(oxygenData[0], 2);
 };
